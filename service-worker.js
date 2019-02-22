08cb1eb3212276b2546f8b0a9c6b915cf81343df
Morten <morten@space10.io>
2019-02-22 21:25:40 +0100

    update path

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-git-practice/build/precache-manifest.836210688193aeed0a9e0a8526bc83ab.js"
+  "/react-git-practice/precache-manifest.ae7eb7be60fbc47d61116f750ec6695e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-git-practice/build/index.html", {
+workbox.routing.registerNavigationRoute("/react-git-practice/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

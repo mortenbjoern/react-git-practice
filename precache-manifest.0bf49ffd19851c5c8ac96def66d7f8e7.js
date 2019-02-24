397bef95cccd27991076e72b3e2c306fadfdcfee
Morten <morten@space10.io>
2019-02-24 20:02:00 +0100

    Updates

--- precache-manifest.0bf49ffd19851c5c8ac96def66d7f8e7.js
+++ precache-manifest.0bf49ffd19851c5c8ac96def66d7f8e7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-git-practice/static/js/runtime~main.d8912df7.js"
   },
   {
-    "revision": "02fc9a6f4b9df5e6674a",
-    "url": "/react-git-practice/static/js/main.02fc9a6f.chunk.js"
+    "revision": "34faf669b7c1fa28a1a5",
+    "url": "/react-git-practice/static/js/main.34faf669.chunk.js"
   },
   {
     "revision": "2dc996b5e902e8d9bda6",
     "url": "/react-git-practice/static/js/2.2dc996b5.chunk.js"
   },
   {
-    "revision": "02fc9a6f4b9df5e6674a",
+    "revision": "34faf669b7c1fa28a1a5",
     "url": "/react-git-practice/static/css/main.260abc26.chunk.css"
   },
   {
-    "revision": "2e5536ec873074944a7b4c604a2d1686",
+    "revision": "7838518602c17ff8f77ad20edf6c7a73",
     "url": "/react-git-practice/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-git-practice/precache-manifest.9142db55cc55eabd5d2874372ef41e5e.js"
+  "/react-git-practice/precache-manifest.0bf49ffd19851c5c8ac96def66d7f8e7.js"
 );
 
 workbox.clientsClaim();

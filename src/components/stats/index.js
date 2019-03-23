969a7d57bee8de9163ddb8f34e57aa2ecebd9c4d
Morten <morten@space10.io>
2019-03-23 13:29:18 +0100

    separated stats component as well

--- src/components/stats/index.js
+++ src/components/stats/index.js
@@ -1,29 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-
-const StatsContainer = styled.div`
-	min-width: 20rem;
-`
-
-const StatsTitle = styled.h2`
-	display: block;
-	font-size: 24px;
-	margin: 0 0 1rem 0;
-`
-
-const StatsItems = styled.div`
-	display: block;
-	& ul {
-		margin: 0;
-		padding: 0;
-		& li {
-			list-style-type: none;
-			text-decoration: none;
-			margin: 0 0 .5rem 0;
-			padding: 0;
-		}
-	}
-`
+import * as Styled from './styles.js';
 
 class Stats extends Component {
 
@@ -39,16 +16,18 @@ class Stats extends Component {
 	} 
 
 	render() {
-		return (<StatsContainer>
-			<StatsTitle>Stats</StatsTitle>
-			<StatsItems>
-				<ul>
-					<li>Test 1</li>
-					<li>Test 2</li>
-					<li>Test 3</li>
-				</ul>
-			</StatsItems>
-		</StatsContainer>);
+		return (
+			<Styled.StatsContainer>
+				<Styled.StatsTitle>Stats</Styled.StatsTitle>
+				<Styled.StatsItems>
+					<ul>
+						<li>Test 1</li>
+						<li>Test 2</li>
+						<li>Test 3</li>
+					</ul>
+				</Styled.StatsItems>
+			</Styled.StatsContainer>
+		);
   	}
 }
 
